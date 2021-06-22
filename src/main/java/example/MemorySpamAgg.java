a88e6d467a97d3fe929ceaa03a733c61e675c2cf
arpitjain <arpit.jain@sprinklr.com>
2021-06-22 11:47:56 +0530

    minor changes in code

--- src/main/java/example/MemorySpamAgg.java
+++ src/main/java/example/MemorySpamAgg.java
@@ -11,13 +11,15 @@ import org.elasticsearch.search.lookup.SearchLookup;
 
 import java.util.*;
 
-public class MemorySpamAgg{
+public class MemorySpamAgg {
     // Assuming each value takes <= 100 bytes
     static final long DEFAULT_WEIGHT = 100;
+
     static public class InitScriptFactory implements ScriptedMetricAggContexts.InitScript.Factory {
         CircuitBreaker circuitBreaker;
-        InitScriptFactory(CircuitBreakerService cbs) {
-            circuitBreaker = cbs.getBreaker(CircuitBreaker.REQUEST);
+
+        InitScriptFactory(CircuitBreakerService circuitBreakerService) {
+            circuitBreaker = circuitBreakerService.getBreaker(CircuitBreaker.REQUEST);
         }
 
         @Override
@@ -30,7 +32,7 @@ public class MemorySpamAgg{
                     state["vals-size"] - size of state["vals"], so that it's size can be known without referencing
                                           the ArrayList. This is needed to reclaim CB.
                      */
-                    state.put("vals", new ArrayList<Object>(){
+                    state.put("vals", new ArrayList<>() {
                         @Override
                         public void finalize() {
                             long sz = XContentMapValues.nodeIntegerValue(state.get("vals-size"), 0);
@@ -46,14 +48,16 @@ public class MemorySpamAgg{
 
     static public class MapScriptFactory implements ScriptedMetricAggContexts.MapScript.Factory {
         CircuitBreaker circuitBreaker;
-        MapScriptFactory(CircuitBreakerService cbs) {
-            circuitBreaker = cbs.getBreaker(CircuitBreaker.REQUEST);
+
+        MapScriptFactory(CircuitBreakerService circuitBreakerService) {
+            circuitBreaker = circuitBreakerService.getBreaker(CircuitBreaker.REQUEST);
         }
 
         @Override
         public ScriptedMetricAggContexts.MapScript.LeafFactory newFactory(Map<String, Object> params, Map<String, Object> state, SearchLookup searchLookup) {
             return leafReaderContext -> new ScriptedMetricAggContexts.MapScript(params, state, searchLookup, leafReaderContext) {
                 final LeafSearchLookup lookup = searchLookup.getLeafSearchLookup(leafReaderContext);
+
                 @Override
                 public void setDocument(int docid) {
                     super.setDocument(docid);
@@ -67,10 +71,10 @@ public class MemorySpamAgg{
                             XContentMapValues.nodeStringValue(params.get("field"))
                     );
                     ArrayList<Object> vals = (ArrayList<Object>) state.get("vals");
-                    for(Object value : scriptDocValues) {
+                    for (Object value : scriptDocValues) {
                         circuitBreaker.addEstimateBytesAndMaybeBreak(DEFAULT_WEIGHT, "my-script/my-expand-map");
-                        long sz = (long)state.get("vals-size");
-                        state.put("vals-size", sz+1);
+                        long sz = (long) state.get("vals-size");
+                        state.put("vals-size", sz + 1);
                         vals.addAll(expand(value));
                     }
                 }
@@ -85,6 +89,7 @@ public class MemorySpamAgg{
             return Collections.singleton(value);
         }
     }
+
     static public class CombineScriptFactory implements ScriptedMetricAggContexts.CombineScript.Factory {
         @Override
         public ScriptedMetricAggContexts.CombineScript newInstance(Map<String, Object> params, Map<String, Object> state) {
@@ -96,34 +101,37 @@ public class MemorySpamAgg{
             };
         }
     }
+
     static public class ReduceScriptFactory implements ScriptedMetricAggContexts.ReduceScript.Factory {
         CircuitBreaker circuitBreaker;
+
         ReduceScriptFactory(CircuitBreakerService cbs) {
             circuitBreaker = cbs.getBreaker(CircuitBreaker.REQUEST);
         }
+
         @Override
         public ScriptedMetricAggContexts.ReduceScript newInstance(Map<String, Object> params, List<Object> states) {
-             return new ScriptedMetricAggContexts.ReduceScript(params, states) {
-                 @Override
-                 public Object execute() {
-                     if(states.isEmpty())
-                         return new ArrayList<>();
-                     final long[] size = {0L};
-                     List<Object> ret = new ArrayList(){
-                         @Override
-                         public void finalize() {
-                             circuitBreaker.addWithoutBreaking(-size[0] * DEFAULT_WEIGHT);
-                         }
-                     };
-                     for(Object state : states) {
-                         List<Object> vals = (ArrayList<Object>) state;
-                         circuitBreaker.addEstimateBytesAndMaybeBreak(vals.size() * DEFAULT_WEIGHT, "my-script/my-expand-reduce");
-                         size[0] += vals.size();
-                         ret.addAll(vals);
-                     }
-                     return ret;
-                 }
-             };
+            return new ScriptedMetricAggContexts.ReduceScript(params, states) {
+                @Override
+                public Object execute() {
+                    if (states.isEmpty())
+                        return new ArrayList<>();
+                    final long[] size = {0L};
+                    List<Object> ret = new ArrayList() {
+                        @Override
+                        public void finalize() {
+                            circuitBreaker.addWithoutBreaking(-size[0] * DEFAULT_WEIGHT);
+                        }
+                    };
+                    for (Object state : states) {
+                        List<Object> vals = (ArrayList<Object>) state;
+                        circuitBreaker.addEstimateBytesAndMaybeBreak(vals.size() * DEFAULT_WEIGHT, "my-script/my-expand-reduce");
+                        size[0] += vals.size();
+                        ret.addAll(vals);
+                    }
+                    return ret;
+                }
+            };
         }
     }
 }
--- src/main/java/example/MyJoinScript.java
+++ src/main/java/example/MyJoinScript.java
@@ -1,69 +1,55 @@
 package example;
 
 import org.apache.http.HttpHost;
-import org.apache.http.util.EntityUtils;
-import org.apache.logging.log4j.LogManager;
-import org.apache.logging.log4j.Logger;
-import org.apache.logging.log4j.core.lookup.EnvironmentLookup;
 import org.apache.lucene.index.LeafReaderContext;
 import org.elasticsearch.ElasticsearchException;
-import org.elasticsearch.action.ActionFuture;
 import org.elasticsearch.action.get.GetResponse;
 import org.elasticsearch.action.get.MultiGetItemResponse;
 import org.elasticsearch.action.get.MultiGetRequest;
 import org.elasticsearch.action.get.MultiGetResponse;
-import org.elasticsearch.client.*;
+import org.elasticsearch.client.Client;
+import org.elasticsearch.client.RestClient;
 import org.elasticsearch.common.Strings;
 import org.elasticsearch.common.breaker.CircuitBreaker;
-import org.elasticsearch.common.util.BigArrays;
-import org.elasticsearch.env.Environment;
 import org.elasticsearch.index.fielddata.ScriptDocValues;
 import org.elasticsearch.script.AggregationScript;
-import org.elasticsearch.search.aggregations.AggregatorBase;
 import org.elasticsearch.search.fetch.subphase.FetchSourceContext;
 import org.elasticsearch.search.lookup.LeafSearchLookup;
 import org.elasticsearch.search.lookup.SearchLookup;
-import org.json.JSONArray;
-import org.json.JSONObject;
 
-import java.net.SocketPermission;
-import java.security.AccessController;
-import java.security.PrivilegedAction;
-import java.util.*;
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Map;
 
 public class MyJoinScript extends AggregationScript {
-    String fkField, indexField, valueField;
-    SearchLookup searchLookup;
-    LeafReaderContext leafCtx;
-//    List<Object> values;
-    List<String> fks;
+    private String fkField, indexField, valueField;
+    private SearchLookup searchLookup;
+    private LeafReaderContext leafReaderContext;
+    //    List<Object> values;
+    private List<String> fks;
     List<Object> values = new LinkedList<>();
-//    BigArrays bigArrays;
-    Client clt;
+    //    BigArrays bigArrays;
+    private  Client esClient;
     CircuitBreaker requestCB;
 
-    private static final Logger logger = LogManager.getLogger(MyJoinScript.class);
     static RestClient client;
     FetchSourceContext fetchSourceContext;
 
-    public MyJoinScript(Client clt, CircuitBreaker requestCB, String fkField, String indexField, String valueField, Map<String, Object> params, SearchLookup searchLookup, LeafReaderContext leafReaderContext) {
+    public MyJoinScript(Client esClient, CircuitBreaker requestCB, String fkField, String indexField, String valueField, Map<String, Object> params, SearchLookup searchLookup, LeafReaderContext leafReaderContext) {
         super(params, searchLookup, leafReaderContext);
-        this.clt = clt;
+        this.esClient = esClient;
         this.requestCB = requestCB;
         this.fkField = fkField;
         this.indexField = indexField;
         this.valueField = valueField;
         this.searchLookup = searchLookup;
-        this.leafCtx = leafReaderContext;
-        if(client == null) {
+        this.leafReaderContext = leafReaderContext;
+        if (client == null) {
             client = RestClient.builder(new HttpHost("localhost", 9200, "http"))
-                   .build();
+                    .build();
         }
         fetchSourceContext = new FetchSourceContext(true, new String[]{valueField}, Strings.EMPTY_ARRAY);
-
-//        BigArrays.NON_RECYCLING_INSTANCE.b
-//        bigArrays = BigArrays.NON_RECYCLING_INSTANCE;
-//        values = bigArrays.newObjectArray(2)
     }
 
     @Override
@@ -73,20 +59,24 @@ public class MyJoinScript extends AggregationScript {
     }
 
     /**
-     * Populate values list from external index's documents corresponding to internal's docid
-     * @param docid
+     * Populate values list from external index's documents corresponding to internal's docId
+     *
+     * @param docId
      */
-    void fetch(int docid) {
+    void fetch(int docId) {
         // The outer "terms" aggregator estimates 5kB per bucket.
         // Estimate the same amount more.
 //        requestCB.addEstimateBytesAndMaybeBreak(2*AggregatorBase.DEFAULT_WEIGHT, "<script my-script>");
 
-        LeafSearchLookup lookup = searchLookup.getLeafSearchLookup(leafCtx);
-        lookup.setDocument(docid);
+        LeafSearchLookup lookup = searchLookup.getLeafSearchLookup(leafReaderContext);
+        lookup.setDocument(docId);
         ScriptDocValues scriptDocValues = MyGroupByPlugin.safeGetScriptDocValues(lookup.doc(), fkField);
-        fks = new LinkedList<String>();
+        if(scriptDocValues == null) {
+            return;
+        }
+        fks = new LinkedList<>();
         values.clear();
-        for(int i = 0; i < scriptDocValues.size(); i++)
+        for (int i = 0; i < scriptDocValues.size(); i++)
             fks.add(scriptDocValues.get(i).toString());
     }
 
@@ -95,44 +85,29 @@ public class MyJoinScript extends AggregationScript {
      */
     private Object fetchExternal() {
         MultiGetRequest request = new MultiGetRequest();
-        for(String fk : fks) {
+        for (String fk : fks) {
             request.add(new MultiGetRequest.Item(indexField, fk).fetchSourceContext(fetchSourceContext));
         }
         List<Object> values = new ArrayList<>(2);
-            try {
-                MultiGetResponse response = clt.multiGet(request).actionGet();
-                for(MultiGetItemResponse irsp : response.getResponses()) {
-                    GetResponse grsp = irsp.getResponse();
-                    if(grsp.isExists() && grsp.getSourceAsMap() != null && grsp.getSourceAsMap().containsKey(valueField)) {
-                        values.add(grsp.getSourceAsMap().get(valueField));
-                    }
+        try {
+            MultiGetResponse response = esClient.multiGet(request).actionGet();
+            for (MultiGetItemResponse multiGetItemResponse : response.getResponses()) {
+                GetResponse getResponse = multiGetItemResponse.getResponse();
+                if (getResponse.isExists() && getResponse.getSourceAsMap() != null && getResponse.getSourceAsMap().containsKey(valueField)) {
+                    values.add(getResponse.getSourceAsMap().get(valueField));
                 }
-            } catch (Exception e) {
-                throw new ElasticsearchException("can't fetch external" + e.getMessage(), e );
             }
-        return values;
-    }
-
-    /**
-     * Make _mget Request JSON string
-     * @param fks list of _id
-     * @return request string
-     */
-    private String makeMultiGetRequest(List<String> fks) {
-//        BigArrays.NON_RECYCLING_INSTANCE.
-        JSONArray ids = new JSONArray();
-        for(String fk : fks) {
-            ids.put(new JSONObject().put("_id", fk));
+        } catch (Exception e) {
+            throw new ElasticsearchException("can't fetch external" + e.getMessage(), e);
         }
-        return new JSONObject().put("docs", ids).toString();
+        return values;
     }
 
     @Override
     public Object execute() {
         try {
             return fetchExternal();
-        }
-        finally {
+        } finally {
 //            requestCB.addWithoutBreaking(-2*AggregatorBase.DEFAULT_WEIGHT);
         }
     }
